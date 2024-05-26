b2bdfee9f11f4a481174960274a7c00fd5661a99
ly <666666>
2024-05-26 14:16:21 +0800

    fix&&feat 服务提供者注解,消费者注解

--- liuyi-rpc/liuyi-annotation/src/main/java/com/liuyi/rpc/annotation/RpcReference.java
+++ liuyi-rpc/liuyi-annotation/src/main/java/com/liuyi/rpc/annotation/RpcReference.java
@@ -14,23 +14,54 @@ import java.lang.annotation.Target;
 public @interface RpcReference {
 
     /**
-     * 接口的class
-     */
-    Class<?> interfaceClass() default Void.class;
-
-    /**
-     * 接口的class名称
-     */
-    String interfaceClassName() default "";
-
-    /**
      * 版本号
      */
-    String version() default "";
+    String version() default "1.0.0";
+
+    /**
+     * 注册中心类型
+     */
+    String registryType() default "zookeeper";
+
+    /**
+     * 注册中心地址
+     */
+    String registryAddress() default "127.0.0.1:2181";
+
+    /**
+     * 负载均衡类型
+     */
+    String loadBalanceType() default "zkconsistenthash";
+
+    /**
+     * 序列化类型
+     */
+    String serializationType() default "protosbuff";
+
+    /**
+     * 超时时间
+     */
+    long timeout() default 5000;
+
+    /**
+     * 是否异步执行
+     */
+    boolean async() default false;
+
+    /**
+     * 是否单向调用
+     */
+    boolean oneWay() default false;
+
+    /**
+     * 代理类型
+     */
+    String proxy() default "jdk";
 
     /**
      * 服务分组
      */
     String group() default "";
+
 
 }
